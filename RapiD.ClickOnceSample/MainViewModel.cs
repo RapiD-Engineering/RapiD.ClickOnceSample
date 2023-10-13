c1805d53a3d9f09cf89bb7f148b2681d02472510
Dirk Kramer <46933603+rapidengineering@users.noreply.github.com>
2023-10-13 14:07:26 +0200

    Add project files.

---
+++ RapiD.ClickOnceSample/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapiD.ClickOnceSample
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string publishedVersion;
+
+        public MainViewModel()
+        {
+            publishedVersion = GetDeploymentVersion();
+        }
+
+        public string GetDeploymentVersion()
+        {
+            //https://learn.microsoft.com/en-us/visualstudio/deployment/access-clickonce-deployment-properties-dotnet?view=vs-2022
+
+            return Environment.GetEnvironmentVariable("ClickOnce_CurrentVersion");
+
+        }
+    }
+}
